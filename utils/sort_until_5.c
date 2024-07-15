037cd49bc628a551e6cb182f1f8b7fd35c40d6ac
Rostyslav Kobeliev <rostyslavkobeliev@gmail.com>
2024-07-15 20:25:19 +0200

    DONE

--- utils/sort_until_5.c
+++ utils/sort_until_5.c
@@ -6,7 +6,7 @@
 /*   By: rkobelie <rkobelie@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 13:10:29 by rkobelie          #+#    #+#             */
-/*   Updated: 2024/07/15 18:55:15 by rkobelie         ###   ########.fr       */
+/*   Updated: 2024/07/15 20:15:30 by rkobelie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,41 +52,6 @@ void	until_3(t_list **list_a)
 		return ;
 	sort_3_elements(list_a, min, next_min);
 }
-/*void	until_3(t_list **list_a)
-{
-	t_list	*head;
-	int		min;
-	int		next_min;
-
-	head = *list_a;
-	min = get_min(list_a, -42);
-	next_min = get_min(list_a, min);
-	if (sort_check(list_a))
-		return ;
-	if (head->index == min && head->index != next_min)
-	{
-		ra(list_a);
-		sa(list_a);
-		rra(list_a);
-	}
-	else if (head->index == next_min)
-	{
-		if (head->next->index == min)
-			sa(list_a);
-		else
-			rra(list_a);
-	}
-	else
-	{
-		if (head->next->index == min)
-			ra(list_a);
-		else
-		{
-			sa(list_a);
-			rra(list_a);
-		}
-	}
-}*/
 
 void	until_4(t_list **list_a, t_list **list_b)
 {
